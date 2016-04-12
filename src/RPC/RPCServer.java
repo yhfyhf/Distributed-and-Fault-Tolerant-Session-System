7caaf1a721595f6dc623d72f0c49dab850de3f0b
Youdan Xu <woshixuyoudan@163.com>
2016-04-11 23:40:18 -0400

    print updated message

--- src/RPC/RPCServer.java
+++ src/RPC/RPCServer.java
@@ -75,7 +75,7 @@ public class RPCServer implements Runnable {
                         System.out.println("!!! Not sure write successfully, outStr: " + outStr);
                         System.out.println("[Server] Now locally the session table is:");
                         for (String sessionKey : SessionTable.sessionTable.keySet()) {
-                            System.out.println(sessionKey + "  " + SessionTable.sessionTable.get(sessionKey));
+                            System.out.println(sessionKey + "  " + SessionTable.sessionTable.get(sessionKey).getMessage());
                         }
                 }
 
