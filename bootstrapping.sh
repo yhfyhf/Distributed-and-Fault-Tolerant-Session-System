d6bd7cb7f41c30e5119c712b155e5b28fd1798a1
yhf <xdhfying@gmail.com>
2016-04-12 20:55:27 -0400

    finish installation script

--- bootstrapping.sh
+++
@@ -1,5 +0,0 @@
-yum -y install tomcat8-webapps tomcat8-docs-webapp tomcat8-admin-webapps  # install tomcat
-curl http://169.254.169.254/latest/meta-data/local-ipv4
-curl http://169.254.169.254/latest/meta-data/ami-launch-index
-
-
